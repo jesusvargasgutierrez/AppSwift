9d2cebf2900f8587d79494cc9ea1aae211017848
jesusvargasgutierrez <jesvargtz_infc@hotmail.com>
2025-09-24 17:04:39 -0600

    cambios en interfaz clientes

--- ClaseOne/Clientes/ApiClientesConnection.swift
+++ ClaseOne/Clientes/ApiClientesConnection.swift
@@ -50,6 +50,48 @@ class ApiClientesConnection {
         
     }
     
+    func CreateClientes(nombre:String,apellido:String,email:String,telefono:String) async throws -> [ClientesModel] {
+        let parametros: [String: Any] = [
+            "EndpointName": "customers",
+            "company_name": nombre,
+            "email": email,
+            "phone": telefono,
+            "country": "",
+            "notes": "",
+        ]
+        
+        guard let clientesURL = URL(string: "https://api.codersweb.com.mx/ws/insertrecord") else {
+            throw ServiceAPIError.invalidURL
+        }
+
+        var request = URLRequest(url: clientesURL)
+        request.httpMethod = "POST"
+        request.timeoutInterval = 15
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        // Serializar a JSON
+        request.httpBody = try JSONSerialization.data(withJSONObject: parametros)
+
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        if let httpResponse = response as? HTTPURLResponse {
+            guard (200..<300).contains(httpResponse.statusCode) else {
+                throw ServiceAPIError.requestFailed(statusCode: httpResponse.statusCode)
+            }
+        }
+
+        guard !data.isEmpty else {
+            throw ServiceAPIError.noData
+        }
+
+        do {
+            let clientes = try JSONDecoder().decode([ClientesModel].self, from: data)
+            return clientes
+        } catch {
+            throw ServiceAPIError.decodingFailed
+        }
+    }
+    
     
     
     enum ServiceAPIError: Error, LocalizedError {
--- ClaseOne/Clientes/NuevoClienteView.swift
+++ ClaseOne/Clientes/NuevoClienteView.swift
@@ -15,42 +15,52 @@ struct NuevoClienteView: View {
     @State private var apellido = ""
     @State private var email = ""
     @State private var telefono = ""
+    @State private var isLoading = false
+    private let apiConnection = ApiClientesConnection()
     
     
     var body: some View {
-        NavigationView{
-            Form{
-                Section("Información Personal"){
+        NavigationView {
+            Form {
+                Section("Información Personal") {
                     TextField("Nombre", text: $nombre)
                     TextField("Apellido", text: $apellido)
                     TextField("Email", text: $email)
                         .keyboardType(.emailAddress)
                     TextField("Teléfono", text: $telefono)
                         .keyboardType(.phonePad)
-                    
                 }
             }
-            
-        }.navigationTitle("Agregar Cliente")
+            .navigationTitle("Agregar Cliente")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
-                ToolbarItem(placement: .navigationBarLeading){
-                    Button("Cancelar"){
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancelar") {
                         dismiss()
                     }
                 }
-                
-                ToolbarItem(placement: .navigationBarTrailing){
-                    
-                    Button("Guardad"){
-                        //LOGICA PARA GUARDAR
-                        dismiss()
-                        
-                        
-                    }.disabled(nombre.isEmpty||apellido.isEmpty)
-                    
+
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Guardar") {
+                        Task {
+                            await handleSend()
+                        }
+                    }
                 }
             }
+        }
+    }
+    
+    func handleSend() async {
+        isLoading = true
+        
+        do{
+            let clientesFromApi = try await apiConnection.CreateClientes(nombre: nombre,apellido:apellido,email:email,telefono:telefono)
+            
+        }catch{
+            print("Error al cargar clientes:\(error.localizedDescription)" )
+        }
+        isLoading = false
     }
 }
 
